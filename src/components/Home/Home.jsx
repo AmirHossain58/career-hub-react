96c81884fbd77213467196147d055fdfba52db04
AmirHossain58 <ahantar58@gmail.com>
2024-03-23 14:07:04 +0600

    basic project setup with tailwind and react router

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi fugit, voluptas quisquam eaque dicta facilis at, expedita rerum dolor nesciunt nemo sequi dolorum ea eum, reiciendis natus sint quaerat laborum.
+        </div>
+    );
+};
+
+export default Home;
